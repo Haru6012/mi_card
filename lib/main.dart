11819810de5720e693482ec2af1701a24acfeebc
松坂波季 <matuharu@matsuzakahakinoMacBook-Pro.local>
2021-10-18 13:42:25 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,40 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
-      ),
-    ),
+      MyApp()
   );
 }
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: NetworkImage("https://pics.prcm.jp/7c11f582cedc6/82088654/jpeg/82088654_465x636.jpeg"),
+              ),
+              Text("JUNG KOOK",
+              style: TextStyle(
+                fontFamily: "Pacifico",
+                fontSize:40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),),
+              Text("Dancer",
+              style: TextStyle(
+                color: Colors.teal.shade100,
+              )),
+            ],
+          )
+        ),
+      ),
+    );
+  }
+}
+
---
+++ lib/test.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child:Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget> [
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color:Colors.white,
+                child: Text("Container 1"),
+              ),
+              Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      height:100.0,
+                      width: 100.0,
+                      color: Colors.blue,
+                      child: Text("Container 2"),
+                    ),
+                    Container(
+                      height:100.0,
+                      width: 100.0,
+                      color: Colors.red,
+                      child: Text("Container 2"),
+                    ),
+                  ]),
+
+              Container(
+                height:100.0,
+                width: 100.0,
+                color: Colors.yellow,
+                child: Text("Container 2"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
